86783dc09a81417adac05e27a6414badfe6eca58
kevashans <kevashans2003@gmail.com>
2023-03-06 15:44:06 +0000

    handshake

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import React, { useState } from 'react';
+
+
 
 function App() {
   //make a get request to localhost:8000/test with axios
+
+  const [nb, setNb ] = useState(0)  
+
+  
+//   this.state = {
+//     filter: []
+// }
   
   async function getArticles() {  // The function is asynchronous
     const articles = (await axios.get('http://localhost:8000/api/test')).data
+    console.log(articles)
     return articles;
   }
 
   return (
     <div className="App">
-      {{getArticles}}
+      
+      {getArticles()}
     </div>
   );
 }
