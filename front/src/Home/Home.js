eb2d245a9470dfa7b3bd74c888c9ea5a011ce173
kevashans <kevashans2003@gmail.com>
2023-03-08 07:54:28 +0000

    basic main page

--- front/src/Home/Home.js
+++ front/src/Home/Home.js
@@ -1,4 +1,4 @@
-// import "./Home.css";
+import "./Home.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -7,7 +7,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Article from "../article/Article.js";
+import Article from "../article/Article";
+import logo from '../image/final_red_1.png'; 
 
 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,25 +18,34 @@ import Article from "../article/Article.js";
 export default function Home() {
     //make a get request to localhost:8000/test with axios
 
-    // const [nb, setNb ] = useState(0)  
+    const [data, setData] = useState([]);
+    
+
+    useEffect(() => {
+        getArticles();
+      }, []);
 
     async function getArticles() {  // The function is asynchronous
-        const articles = (await axios.get('http://localhost:8000/api/test')).data
-        console.log(articles)
-        return articles;
+        const articles = JSON.parse((await axios.get('http://localhost:8000/api/article')).data)
+        // console.log(articles)
+        setData(articles)
+        // console.log(data)
+       
     }
     return (
         <>
-
+            {/* {console.log(data)} */}
             <div>
+            
+
                 <Navbar bg="light" expand="lg">
                     <Container>
-                        <Navbar.Brand href="#home">Sport Savvy</Navbar.Brand>
+                        <Navbar.Brand href="#home"><img className="logo" src={logo} /></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
                                 <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
+                                <Nav.Link href="/Login">Link</Nav.Link>
                                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">
@@ -52,9 +62,20 @@ export default function Home() {
                     </Container>
                 </Navbar>
 
-                <div className='articles mt-5'>
+                {data.length ? data.map(n => <Article key={n.id} title={n.title} content={n.content} thumbnail={n.thumbnail}></Article>) : <div>LOADING</div>}
+
+                 
+
+
+                {/* {data.map( n => console.log(n))} */}
+
+
+              {/* <Article name={data.title}></Article> */}
+          
+
+                {/* <div className='articles mt-5'>
                 <Article></Article>
-                </div>
+                </div> */}
 
             </div>
         </>
--- front/src/article/Article.js
+++ front/src/article/Article.js
@@ -1,58 +1,75 @@
 // import "./Home.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./ArticleDetail";
 
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import ArticleDetail from "./ArticleDetail";
 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 // import { Route, Link, Routes, useNavigate } from "react-router-dom";
 
-export default function Article() {
+export default function Article(props) {
     //make a get request to localhost:8000/test with axios
 
-    const [data, setData] = useState(undefined);
-    const [tags, setTags] = useState([]);
+    const [data, setData] = useState([]);
+    // const [tags, setTags] = useState([]);
+
+let array = [{"id":1,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T11:34:08.000000Z","updated_at":"2023-03-07T11:34:08.000000Z"},{"id":2,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T18:41:30.000000Z","updated_at":"2023-03-07T18:41:30.000000Z"},{"id":3,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T18:41:30.000000Z","updated_at":"2023-03-07T18:41:30.000000Z"},{"id":4,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T18:41:30.000000Z","updated_at":"2023-03-07T18:41:30.000000Z"},{"id":5,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T18:41:31.000000Z","updated_at":"2023-03-07T18:41:31.000000Z"},{"id":6,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T18:41:31.000000Z","updated_at":"2023-03-07T18:41:31.000000Z"},{"id":7,"title":"Welcome to ecoal23","content":"<h1>Hello from ecoal<\/h1><p>Nice to see you in <strong>Lens<\/strong>. Enjoy !<\/p>","thumbnailURL":"\/ecoal.jpg","mediaType":"image","mediaURL":"\/ecoal.jpg","leadStory":0,"created_at":"2023-03-07T18:41:31.000000Z","updated_at":"2023-03-07T18:41:31.000000Z"}]
+
+
 
 
     // useEffect(() => {
     //     getArticles();
     // }, []);
-
+    // // let array = [1, 2, 3]
+    
+    
     // async function getArticles() {  // The function is asynchronous
-    //     const articles = (await axios.get('http://localhost:8000/api/test')).data
-    //     console.log(articles)
-    //     return articles;
+    //     const articles = JSON.parse((await axios.get('http://localhost:8000/api/article')).data)
+    //     // console.log(articles)
+    //     setData(articles)
+    //     // console.log(data)
+
     // }
+
+
+
+
     return (
         <>
 
+            {console.log(props.title)}
             <div>
-                {/* HoME
-            {getArticles()} */}
 
-                <Row xs={1} md={2} className="g-4">
-                    {Array.from({ length: 4 }).map((_, idx) => (
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top" src="holder.js/100px160" />
-                                <Card.Body>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text>
-                                        This is a longer card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit
-                                        longer.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
+            <Row xs={1} md={2} className="g-4">
+                    
+                    <Col className="mt-5">
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>{props.title}</Card.Title>
+                                <Card.Text>
+                                    {props.content}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                
+            </Row>
+                
 
 
             </div>
+
+            
+
+            {/* {array.map( n => console.log(n.id))} */}
+            {/* {data.map( n => console.log(n.id))} */}
         </>
     );
 }
